76c7c98d62f22facfc4a0d7a23ecc802b65ee357
José Mota <jose@josemota.net>
2013-07-03 18:33:58 +0100

    Allow to change the end input.

--- js/views/range.js
+++ js/views/range.js
@@ -3,10 +3,17 @@ var RangeView = Backbone.View.extend({
     var template = $("#range-template").html();
     this.$el.html($.mustache(template,this.model.toJSON()));
     return this;
-  } 
+  }
 , events: {
+    "change .plan-end"  : "endChanged"
   , "click .plan-remove" : "remove"
   }
+
+, endChanged: function(e) {
+    var value = parseInt($(e.target).val());
+    this.model.set("end", value);
+  }
+
 , remove: function() {
     this.model.trigger("wantsToRemove", this.model);
   }
