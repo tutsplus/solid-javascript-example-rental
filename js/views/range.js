36e90a145264d74652c9bf53efe61a92b1f47910
José Mota <jose@josemota.net>
2013-07-21 16:34:46 +0100

    If max range, replace with "--".

--- js/views/range.js
+++ js/views/range.js
@@ -4,7 +4,7 @@ var RangeView = Backbone.View.extend({
 , render: function() {
     var template    = $("#range-template").html();
     var hour_ranges = this.renderHourRanges();
-    var json        = this.model.toJSON();
+    var json        = this.transformModel();
 
     this.$el.html($.mustache(template, json));
     this.$el.find(".plan-24").append(hour_ranges);
@@ -29,5 +29,18 @@ var RangeView = Backbone.View.extend({
     return new HourRangeListView({
       collection: this.model.get("hour_ranges")
     }).render().el;
+  }
+
+, transformModel: function() {
+    var duplicateModel = _.clone(this.model);
+
+    if(duplicateModel.get("end") == Range.MAX) {
+      duplicateModel.set("end", "--", { silent: true });
+    }
+
+    var json = duplicateModel.toJSON();
+    delete duplicateModel;
+
+    return json;
   }
 });
